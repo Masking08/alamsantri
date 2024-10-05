5d48eca8f53170d4cd8a722ca59823322dcf6ee3
Masking08 <149226877+Masking08@users.noreply.github.com>
2024-10-06 02:46:10 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,24 +18,12 @@ document.addEventListener('click', function (e) {
     }
 });
 
-// SMOOTH SCROLLING
+// CONTACT
+function sendMessage() {
+    const name = document.getElementById('name').value;
+    const message = document.getElementById('message').value;
+    const nomor = document.getElementById('nomor').value;
 
-const navlink = document.querySelectorAll(".navbar a");
-navlink.forEach(elem => elem.addEventListener("click", smoothscroll));
-function smoothscroll(event){
-    event.preventDefault();
-    const targetId = event.currentTarget.getAttribute("href");
-    window.scrollTo({
-        top: document.querySelector(targetId).offsetTop,
-        behavior: "smooth"
-    })
-}
-
-        function sendMessage() {
-            const name = document.getElementById('name').value;
-            const message = document.getElementById('message').value;
-            const nomor = document.getElementById('nomor').value;
-
-            const url = "https://api.whatsapp.com/send?phone=6283869236077&text=Halo%20Admin%0ASaya%20*+"+ name +"*%0A*"+ message+ "*%0ANomor%20Saya%20*" + nomor +"*%0A";
-            window.open(url);
-        }
+    const url = "https://api.whatsapp.com/send?phone=6283869236077&text=Halo%20Admin%0ASaya%20*+"+ name +"*%0A*"+ message+ "*%0ANomor%20Saya%20*" + nomor +"*%0A";
+    window.open(url);
+};
